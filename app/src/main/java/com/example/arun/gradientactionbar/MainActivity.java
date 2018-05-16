6a559a6dbb7925017ddb7e3645b18096bfcc2c4a
arunpayyeri <arunpayyeri@gmail.com>
2018-05-16 21:58:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arun/gradientactionbar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.arun.gradientactionbar;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_actionbar));
+    }
+}
